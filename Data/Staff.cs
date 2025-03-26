5a9f9c28c5e3834b34901ccc9aecc24752ae1fda
Sherif Hamdy <sherif_hfm@yahoo.com>
2025-03-26 14:20:55 +0300

    updates

--- Data/Staff.cs
+++ Data/Staff.cs
@@ -1,9 +1,13 @@
-﻿namespace AcademicYearCalendar.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AcademicYearCalendar.Data
 {
     public class Staff
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "يجب ادخال اسم عضو هيئة التدريس")]
+        [Display(Name = "اسم عضو هيئة التدريس")]
         public string StaffName { get; set; }
     }
 }
