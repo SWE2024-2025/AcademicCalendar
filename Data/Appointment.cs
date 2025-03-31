ecf969bd20cb3cb5bc191113401660c7994a27cb
Sherif Hamdy <sherif_hfm@yahoo.com>
2025-03-31 21:03:27 +0300

    updates

--- Data/Appointment.cs
+++ Data/Appointment.cs
@@ -1,4 +1,6 @@
-﻿namespace AcademicYearCalendar.Data
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using DevExpress.Blazor;
+namespace AcademicYearCalendar.Data
 {
     public class Appointment
     {
@@ -18,6 +20,79 @@
         public string? Resources { get; set; }
         public string? TextColor { get; set; }
         public string? BackgroundColor { get; set; }
-        
+
+        private DxSchedulerRecurrenceInfo dxSchedulerRecurrenceInfo;
+
+        [NotMapped]
+        public string RepeatEndDate
+        {
+            get
+            {
+                if (IsRecurrence)
+                {
+                    setDxSchedulerRecurrenceInfo();
+                    return dxSchedulerRecurrenceInfo.End.ToString("yyyy-MM-dd");
+                }
+                return "";
+            }
+        }
+
+        [NotMapped]
+        public string RepeatDay
+        {
+            get
+            {
+                if (IsRecurrence)
+                {
+                    setDxSchedulerRecurrenceInfo();
+                    if (dxSchedulerRecurrenceInfo.Type == SchedulerRecurrenceType.Daily)
+                    {
+                        return " يوميا ";
+                    }
+                    else if (dxSchedulerRecurrenceInfo.Type == SchedulerRecurrenceType.Weekly)
+                    {
+                        switch (dxSchedulerRecurrenceInfo.WeekDays)
+                        {
+                            case SchedulerWeekDays.Saturday:
+                                return "سبت";
+                            case SchedulerWeekDays.Sunday:
+                                return "احد";
+                            case SchedulerWeekDays.Monday:
+                                return "اثنين";
+                            case SchedulerWeekDays.Tuesday:
+                                return "ثلاثاء";
+                            case SchedulerWeekDays.Wednesday:
+                                return "اربعاء";
+                            case SchedulerWeekDays.Thursday:
+                                return "خميس";
+                            case SchedulerWeekDays.Friday:
+                                return "جمعة";
+                        }
+                    }
+                    else if(dxSchedulerRecurrenceInfo.Type == SchedulerRecurrenceType.Monthly)
+                    {
+                        return dxSchedulerRecurrenceInfo.DayNumber.ToString() + " من كل شهر ";
+                    }
+                }
+                return "";
+            }
+        }
+
+        private bool IsRecurrence
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(Recurrence);
+            }
+        }
+
+        private void setDxSchedulerRecurrenceInfo()
+        {
+            if (dxSchedulerRecurrenceInfo != null || !IsRecurrence)
+                return;
+            dxSchedulerRecurrenceInfo=new DxSchedulerRecurrenceInfo();
+            dxSchedulerRecurrenceInfo.FromXml(Recurrence);
+        }
+
     }
 }
